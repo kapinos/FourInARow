00fcd4a63d66f17b4a62f6451a0c8b35ac7a2c13
ankis <anastasia.kapinos@gmail.com>
2017-11-09 18:18:32 +0200

    Improved calculating of field and positions of balls. Implemented adding views on the last free row in a field

--- FourInARow/BattleFieldViewController.swift
+++ FourInARow/BattleFieldViewController.swift
@@ -8,36 +8,59 @@
 
 import UIKit
 
-struct Ball {
-    var x   = -1.0
-    var y   = -1.0
-    var row = -1
-    var col = -1
+struct Position {
+    var row = 0
+    var col = 0
     
-    init() { }
-    
-    init(x: Double, y: Double, row: Int, col: Int) {
-        self.x = x
-        self.y = y
+    init(row: Int, col: Int) {
         self.row = row
         self.col = col
     }
 }
 
+struct Ball {
+    var point: CGPoint = CGPoint(x: -1, y: -1)
+    var position = Position(row: -1, col: -1)
+    var size = Sizes.BallSize
+    
+    init() { }
+    
+    init(point: CGPoint, position: Position) {
+        self.point = point
+        self.position = position
+    }
+}
+
+enum Player: String {
+    case first  = "first"
+    case second = "second"
+    case none   = "none"
+}
+
+struct BallView {
+    var ball = Ball()
+    var player = Player.none
+    
+    init() { }
+    
+    init(ball: Ball) {
+        self.ball = ball
+    }
+}
+
 struct Sizes {
-    static var BallSize     = -1
-    static let Inset        = 10
+    static var BallSize: CGFloat    = -1
+    static let Inset: CGFloat       = 5
     static let COLS         = 6
     static var ROWS         = -1
-    static var NavBarHeight:CGFloat = -1.0
 }
 
 class BattleFieldViewController: UIViewController, UIGestureRecognizerDelegate {
 
     // MARK: - Properties
-     var arrayBalls: [[Ball]] = []
+    var arrayBalls: [[Ball]] = []
+    var arrayBallViews: [[BallView]] = []
 
-    
     // MARK: - IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     
@@ -52,27 +75,28 @@ class BattleFieldViewController: UIViewController, UIGestureRecognizerDelegate {
         super.viewWillLayoutSubviews()
         
         configureGeometryParameters()
-        configureBalls()
-       // printArray()
     }
 
     // MARK: - Inner Methods
     
     private func configureGeometryParameters() {
-        Sizes.NavBarHeight = self.navigationController!.navigationBar.frame.size.height
         
-        // count balls size from the width
-        Sizes.BallSize = Int(round((self.view.bounds.width - CGFloat(Sizes.Inset)) / CGFloat(Sizes.COLS) - CGFloat(Sizes.Inset)))
+        // calculate balls size from the width
+        Sizes.BallSize = ((self.imageView.bounds.width - Sizes.Inset) / CGFloat(Sizes.COLS)) - Sizes.Inset
         
-        // count amount of rows from the balls size
-        let amountRows = (self.view.bounds.height - CGFloat(Sizes.Inset) - Sizes.NavBarHeight) / CGFloat(Sizes.BallSize + Sizes.Inset)
+        // calculate amount of rows from the balls size
+        let amountRows = (self.imageView.bounds.height - Sizes.Inset) / (Sizes.BallSize + Sizes.Inset)
+        print("amountRows \(amountRows)")
         Sizes.ROWS = Int(floor(amountRows))
         
-        //print("ballSize = \(Sizes.BallSize), rows = \(Sizes.ROWS)\nwidth=\(self.view.bounds.width), height=\(self.view.bounds.height)")
+        print("ballSize = \(Sizes.BallSize), rows = \(Sizes.ROWS)\nwidth=\(self.imageView.bounds.width), height=\(self.imageView.bounds.height)")
         
-        // init array of balls
+        // init array of balls !!!!!! simple
         arrayBalls = Array(repeating: Array(repeating: Ball(), count: Sizes.COLS),
                            count: Sizes.ROWS)
+        arrayBallViews = Array(repeating: Array(repeating: BallView(), count: Sizes.COLS),
+                               count: Sizes.ROWS)
+        configureBalls()
     }
     
     private func configureTapGestureRecognizer() {
@@ -89,50 +113,60 @@ class BattleFieldViewController: UIViewController, UIGestureRecognizerDelegate {
     
     func configureBalls()  {
         for row in 0..<Sizes.ROWS {
-            var arr = [Ball]()
+            var arrBalls = [Ball]()
+            var arrBallViews = [BallView]()
             for col in 0..<Sizes.COLS {
-                arr.append(Ball(x: Double((Sizes.Inset + Sizes.BallSize) * col + Sizes.Inset),
-                                y: Double((Sizes.Inset + Sizes.BallSize) * row + Sizes.Inset),
-                                row: row, col: col))
+                let ball = Ball(point: CGPoint(x: (Sizes.Inset + Sizes.BallSize) * CGFloat(col) + Sizes.Inset,
+                                               y: (Sizes.Inset + Sizes.BallSize) * CGFloat(row) + Sizes.Inset),
+                                position: Position(row: row, col: col))
+                arrBalls.append(ball)
+                arrBallViews.append(BallView(ball: ball))
             }
-            arrayBalls[row] = arr
+            arrayBalls[row] = arrBalls
+            arrayBallViews[row] = arrBallViews
         }
     }
     
-    func printArray() {
-        for row in 0..<Sizes.ROWS {
-            for col in 0..<Sizes.COLS {
-                print("\(row) \(col), x:\(arrayBalls[row][col].x), y:\(arrayBalls[row][col].y)")
-            }
-        }
-    }
     
     func checkTouch(_ touchPoint: CGPoint) {
         
-        let touchX = touchPoint.x
-        let touchY = touchPoint.y
-        
-        print("x: \(touchX), y: \(touchY)")
+        print("x: \(touchPoint.x), y: \(touchPoint.y)")
         
         for row in 0..<Sizes.ROWS {
             for col in 0..<Sizes.COLS {
-                let minX = CGFloat(arrayBalls[row][col].x)
-                let maxX = minX + CGFloat(Sizes.BallSize)
                 
-                let minY = CGFloat(arrayBalls[row][col].y)
-                let maxY = minY + CGFloat(Sizes.BallSize)
-                
-                if (minX <= touchX && touchX <= maxX) && ( minY <= touchY && touchY <= maxY) {
-                    let view = UIView(frame: CGRect(x: arrayBalls[row][col].x,
-                                                    y: arrayBalls[row][col].y,
-                                                    width: Double(Sizes.BallSize),
-                                                    height: Double(Sizes.BallSize)))
-                    view.backgroundColor = UIColor.darkGray
+                let rect = CGRect(origin: touchPoint, size: CGSize(width: Sizes.BallSize, height: Sizes.BallSize))
+
+                // find the position of ball for tap
+                if !rect.contains(touchPoint) {
+                    continue
+                } else {
+                    let rowForSet = getLastFreeRow(tappedRow: row, tappedCol: col)
+            
+                    arrayBallViews[rowForSet][col].player = Player.first
+                    
+                    let ball = arrayBallViews[rowForSet][col].ball
+                    let view = UIView(frame: CGRect(origin: ball.point,
+                                                    size: CGSize(width: ball.size, height: ball.size)))
+                    
+                    view.backgroundColor = UIColor.blue
                     self.imageView.addSubview(view)
                 }
             }
         }
     }
 
+   private func getLastFreeRow(tappedRow: Int, tappedCol: Int) -> Int {
+        var rowForBall = tappedRow
+        
+        for r in tappedRow..<Sizes.ROWS {
+            if arrayBallViews[r][tappedCol].player == .none {
+                rowForBall = r
+            } else {
+                break
+            }
+        }
+        return rowForBall
+    }
 }
 
